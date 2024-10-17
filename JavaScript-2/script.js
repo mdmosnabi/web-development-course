d8a9285bf7ca69c9bd54b22355819ec53ae02dea
md:mosnabi <mdmosnabi456@gmail.com>
2024-10-17 19:16:00 +0600

    last update

---
+++ JavaScript-2/script.js
@@ -0,0 +1,22 @@
+// let a = 10;
+// console.log(a);
+
+//  var b = 20
+//  console.log(b);
+
+//  const c = 30;
+//  console.log(c);
+ 
+//  d = a+b+c;
+// console.log(d);
+
+// a=30
+// console.log(a);
+
+// c=20;
+// console.log(c);
+
+let a = 10;
+let A = 5
+
+console.log(a,A);
---
+++ Javascript-1/script.js
@@ -0,0 +1,7 @@
+console.log('hello world')
+
+// i am single line comment 
+
+/* 
+let a = 'i am a line'
+console.log(a) */
