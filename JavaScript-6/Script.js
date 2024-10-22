8082c7760ac464361f4896f9e8bd1bd8e5dff406
md:mosnabi <mdmosnabi456@gmail.com>
2024-10-22 09:58:37 +0600

    javaScript tutorial

---
+++ JavaScript-6/Script.js
@@ -0,0 +1,19 @@
+// Example of Primitive
+
+let a = null
+
+// console.log(typeof a);
+
+//Example of Non-Primitive
+
+let b = {key1:12 , key2:5645 , key3:(2+3)}
+
+console.log(typeof b);
+console.log(b.key3);
+
+
+// use of BackTick/Backquate
+let name = 'Ratul'
+let c = `my name is ${name}`
+
+console.log(c);
---
+++ JavaScript-7/script.js
@@ -0,0 +1,18 @@
+// js Functions
+
+function add(a,b){
+    return a+b
+}
+
+console.log(add(1,5));
+console.log(add(58,8));
+
+const sum = (a,b)=>{return a*b}
+
+console.log(sum(578,5));
+
+function sub(a,b) {
+    return a-b
+}
+
+console.log(sub(5));
